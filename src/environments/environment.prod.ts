2403ca7336f151afd9154233913ec7b799563ab0
Serj <sv@spiks.ru>
2020-05-06 15:07:51 +0300

    Create post page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyBM7-nKb1zodmJZLOGVh1ZGGK3E9bcMRR0'
+  apiKey: 'AIzaSyBM7-nKb1zodmJZLOGVh1ZGGK3E9bcMRR0',
+  fbDbUrl: 'https://angular-blog-d228e.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyBM7-nKb1zodmJZLOGVh1ZGGK3E9bcMRR0'
+  apiKey: 'AIzaSyBM7-nKb1zodmJZLOGVh1ZGGK3E9bcMRR0',
+  fbDbUrl: 'https://angular-blog-d228e.firebaseio.com'
 };
 
 /*
